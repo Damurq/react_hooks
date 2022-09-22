510f6172c5c9c0b3797fd21db90d701e8f0ad0ae
Michael David Montero <61468376+Damurq@users.noreply.github.com>
2022-09-21 21:56:38 -0500

    hooks mas comunes

---
+++ useContext_example.jsx
@@ -0,0 +1,38 @@
+// valor inicial
+const themes = {
+    light: {
+      foreground: "#000000",
+      background: "#eeeeee"
+    },
+    dark: {
+      foreground: "#ffffff",
+      background: "#222222"
+    }
+  };
+  //Creo el contexto
+  const ThemeContext = React.createContext(themes.light);
+  // Paso el provider
+  function App() {
+    return (
+      <ThemeContext.Provider value={themes.dark}>
+        <Toolbar />
+      </ThemeContext.Provider>
+    );
+  }
+
+  function Toolbar(props) {
+    return (
+      <div>
+        <ThemedButton />
+      </div>
+    );
+  }
+    // Utilizo useContext para obtener el estado  
+  function ThemedButton() {
+    const theme = useContext(ThemeContext);
+    return (
+      <button style={{ background: theme.background, color: theme.foreground }}>
+        I am styled by theme context!
+      </button>
+    );
+  }
---
+++ useState_example.jsx
@@ -0,0 +1,37 @@
+// class
+class Example extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        count: 0
+      };
+    }
+  
+    render() {
+      return (
+        <div>
+          <p>You clicked {this.state.count} times</p>
+          <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+            Click me
+          </button>
+        </div>
+      );
+    }
+  }
+
+// Hook
+import React, { useState } from 'react';
+
+function Example() {
+  // Declaración de una variable de estado que llamaremos "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
